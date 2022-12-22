1a618b2fa4e16cc1fe8ce044477b68cdde63f4ea
rakshya2001 <bhattarakshya6@gmail.com>
2022-12-22 22:40:39 +0545

    dashboard screen

--- roomandu/lib/Screen/screeen/dashboard_screen.dart
+++ roomandu/lib/Screen/screeen/dashboard_screen.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:roomandu/Screen/screeen/login_screen.dart';
+import 'package:roomandu/Screen/screeen/register_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -8,8 +10,28 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  final int _selectIndex=0;
+
+  List<Widget> lstBottomScreen=[
+    const LoginScreen(),
+    RegisterScreen()
+
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+      ),
+      body: lstBottomScreen[_selectIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            ),
+        ],
+      ),
+    );
   }
 }
