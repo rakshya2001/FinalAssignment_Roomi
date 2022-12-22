1c09bc37450092d9e4f4769ccb29135036b7fc68
rakshya2001 <bhattarakshya6@gmail.com>
2022-12-22 11:47:54 +0545

    Initial Commit

---
+++ roomandu/android/app/src/main/kotlin/room/com/roomandu/MainActivity.kt
@@ -0,0 +1,6 @@
+package room.com.roomandu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ roomandu/lib/main.dart
@@ -0,0 +1,93 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:roomandu/Screen/screeen/login_screen.dart';
+void main() {
+  runApp(
+    MaterialApp(
+      initialRoute: '/login',
+      routes: {
+        '/login':(context) =>  const ClassLoginScreen()
+
+      },
+    debugShowCheckedModeBanner: false,
+    
+    ),
+    );
+}
+
+// class MeroClassScreen extends StatelessWidget {
+//   const MeroClassScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: const Color.fromARGB(255, 237, 167, 227),
+//       appBar: AppBar(
+//         title:const Text('first app'),
+//         backgroundColor: const Color.fromARGB(255, 90, 167, 203),
+
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Container(
+          
+//           width: double.infinity,
+//           color: Colors.amber,
+//           child: const Text('hello world',
+//           style: TextStyle(
+//             fontSize: 30,
+//             fontWeight: FontWeight.bold,
+//             color:Color.fromARGB(255, 186, 71, 62),
+//             backgroundColor: Color.fromARGB(255, 167, 174, 235)
+//           ),),
+//         ),
+//       ),
+      
+      // body:RichText(
+      //   text: const TextSpan(
+      //     text: 'Hello my name is riya ',
+      //     style: TextStyle(
+      //       color: Colors.black,
+      //       fontSize: 30,
+      //     ),
+      //     children: <TextSpan>[
+      //       TextSpan(
+      //         text: 'bold',
+      //         style: TextStyle(
+      //           fontWeight: FontWeight.bold,
+      //           color: Colors.white,
+      //         ),
+      //       ),
+      //       TextSpan(
+      //         text: 'world! \n',
+      //         style: TextStyle(
+      //           color: Colors.red,
+      //           fontStyle: FontStyle.italic,
+      //         ),
+      //       ),
+      //       TextSpan(
+      //         text: 'The future belongs to those who prepare for it today.',
+      //       ),
+      //     ],
+      //   ),
+      // )
+
+      // body: const TextField(
+      //   decoration: InputDecoration(
+      //     border: OutlineInputBorder(
+      //       borderRadius: BorderRadius.all(Radius.circular(10.0))
+      //     ), 
+      //     // kehi lekhda kheri outline border change huncha matlab purple ma hudaina 
+      //     enabledBorder: OutlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.purple,width: 5),
+      //     ),
+      //     // tap garda tehi color aauna lai 
+      //     focusedBorder: OutlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.purple,width: 5)
+      //     ),
+//       //     labelText: 'Enter your name '
+//         );
+//   }
+// }
